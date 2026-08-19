83afb009ccdd681f150ed4cdfcf5539bb520cb6b
eino ojala <eino.h.ojala@gmail.com>
2026-08-19 13:51:47 +0300

    hi

---
+++ mod3/tehtävä2.py
@@ -0,0 +1,5 @@
+import math
+
+sade = float(input( "Anna säde: "))
+ala = math.pi * sade ** 2
+print("pinta-ala on", ala)
---
+++ mod3/tehtävä3.py
@@ -0,0 +1,10 @@
+import math
+
+korkeus = float(input("Anna korkeus: "))
+kanta = float(input("Anna kanta: "))
+
+piiri = 2 * korkeus + 2 * kanta
+ala = kanta * korkeus
+
+print("Piiri on", piiri)
+print("Ala on", ala)
