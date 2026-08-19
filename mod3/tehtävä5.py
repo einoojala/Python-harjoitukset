6f89b7e98d058e1c1b3d1d97671261f01e691c00
eino ojala <eino.h.ojala@gmail.com>
2026-08-19 14:42:21 +0300

    1

--- mod3/tehtävä5.py
+++ mod3/tehtävä5.py
@@ -2,10 +2,8 @@ leiviskat = float(input("Anna leiviskät.\n"))
 naulat = float(input("Anna naulat.\n"))
 luodit = float(input("Anna luodit.\n"))
 
-# Muutetaan kaikki luodeiksi
 luodit_yhteensa = leiviskat * 20 * 32 + naulat * 32 + luodit
 
-# Yksi luoti painaa 13,3 grammaa
 grammat = luodit_yhteensa * 13.3
 
 kilogrammat = int(grammat // 1000)
